a6ad157aaad74a983e45ce6bcd9ddde3b574332c
Akim de Jesus Galicia <akimdjg@gmail.com>
2022-09-07 15:51:18 -0500

    add Streams Example

---
+++ example/src/com/practice/java/streams/StreamsExample.java
@@ -0,0 +1,22 @@
+package example.src.com.practice.java.streams;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import example.src.com.practice.java.FuntionalInterfaces.PredicateAndConsumerExample;
+import example.src.com.practice.java.data.User;
+import example.src.com.practice.java.data.UserDataBase;
+
+public class StreamsExample {
+
+    private static List<User> users= UserDataBase.getAllUsers();
+    public static void main(String[] args) {
+        Map<String, LocalDate> mUser = users.stream()
+            .filter(PredicateAndConsumerExample.pAge.and(PredicateAndConsumerExample.pGen))
+            .collect(Collectors.toMap(User::getName, User::getBirthday));
+
+        System.out.println(mUser);
+    }
+}
