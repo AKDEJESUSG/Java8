67b9a3f032f8b496ebd22d9cc6c2914877ff9403
Akim de Jesus Galicia <akimdjg@gmail.com>
2022-09-08 17:16:23 -0500

    add collections and streams diferents add how to debug streams using peek

--- example/src/com/practice/java/streams/StreamsExample.java
+++ example/src/com/practice/java/streams/StreamsExample.java
@@ -14,7 +14,9 @@ public class StreamsExample {
     private static List<User> users= UserDataBase.getAllUsers();
     public static void main(String[] args) {
         Map<String, LocalDate> mUser = users.stream()
+            .peek((u->System.out.println("--"+u.getName())))
             .filter(PredicateAndConsumerExample.pAge.and(PredicateAndConsumerExample.pGen))
+            .peek((u->System.out.println("**"+u.getName())))
             .collect(Collectors.toMap(User::getName, User::getBirthday));
 
         System.out.println(mUser);
